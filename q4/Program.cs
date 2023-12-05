d3970b0ec34110c92a564febf20b5a0d9ebcb413
David Zwart <davidzwa@gmail.com>
2023-12-05 20:59:27 +0100

    feat: finish q4

--- q4/Program.cs
+++ q4/Program.cs
@@ -4,11 +4,11 @@
     "example.txt",
     "test1.txt",
 };
-string filePath = files[1];
+string filePath = files[0];
 List<string> fileContent = File.ReadLines(filePath).ToList();
 
 var sumTotalCards = 0;
-List<int> cardScores = new();
+List<int> cardWinningNumberCount = new();
 foreach (var cards in fileContent)
 {
     var indexGame = cards.Split(": ").ToList();
@@ -18,19 +18,36 @@ foreach (var cards in fileContent)
     {
         throw new Exception("invalid parse output");
     }
-    
+
     var winners = winAndContents[0].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
     var numbers = winAndContents[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
     var cardWins = winners.Sum(w => numbers.Contains(w) ? 1 : 0);
     var cardScore = (int)Math.Pow(2, cardWins - 1);
 
     sumTotalCards += cardScore;
-    cardScores.Add(cardWins);
+    cardWinningNumberCount.Add(cardWins);
     Console.WriteLine($"gameIndex {gameIndex} cardWins: {cardWins} (score: {cardScore})");
 }
-
-List<int> cardCounts = 1..4;
 
 Console.WriteLine(sumTotalCards);
 // 18619 correct
 
+List<int> cardCounts = Enumerable.Repeat(1, cardWinningNumberCount.Count).ToList();
+for (var i = 0; i < cardCounts.Count; i++)
+{
+    var reps = cardWinningNumberCount[i];
+    if (i == cardCounts.Count - 1)
+        continue;
+
+    // 0-based, inclusive max
+    var endIndex = Math.Min(i + reps, cardCounts.Count - 1);
+    Console.WriteLine($"i: {i} reps: {reps} endIndex: {endIndex}");
+    for (var j = i + 1; j <= endIndex; j++)
+    {
+        cardCounts[j] += cardCounts[i];
+        Console.WriteLine($"j {j} reps {reps}");
+    }
+}
+
+// 1, 2, 4, 8, 14, 1
+Console.WriteLine(cardCounts.Sum());
