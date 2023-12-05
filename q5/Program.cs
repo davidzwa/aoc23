cc75e4c0312d296fa7afad2e70a679fb0c4e927b
David Zwart <davidzwa@gmail.com>
2023-12-05 23:26:35 +0100

    fix: attempt case 2, but I need to tackle it in a smarter way

--- q5/Program.cs
+++ q5/Program.cs
@@ -4,20 +4,11 @@
     "example.txt",
     "test1.txt",
 };
-string filePath = files[0];
+string filePath = files[1];
 List<string> fileContent = File.ReadLines(filePath).ToList();
 
 List<long> seeds = new();
 
-List<string> types = new()
-{
-    "soil",
-    "fertilizer",
-    "water",
-    "light",
-    "temperature",
-    "humidity",
-};
 List<Map> mappings = new();
 
 (string Source, string Target)? processingMap = null;
@@ -57,10 +48,8 @@ foreach (var line in fileContent)
 List<List<(long Input, long Output)>> seedIO = new();
 List<long> locations = new();
 
-foreach (var seed in seeds)
+long ProcessLocations(long seed)
 {
-    // var lastStage = stages.LastOrDefault();
-
     var lastInputOutputs = new List<(long Input, long Output)> { (Input: seed, Output: 0L) };
     seedIO.Add(lastInputOutputs);
 
@@ -83,15 +72,78 @@ foreach (var seed in seeds)
         {
             if (mapping.Link.Target == "location")
             {
-                locations.Add(lastInputOutput.Output);
+                return lastInputOutput.Output;
             }
+
             Console.WriteLine(
                 $"{mapping.Link.Source} {lastInputOutput.Input}, {mapping.Link.Target} {lastInputOutput.Output}");
         }
     }
+
+    return default;
+}
+
+foreach (var seed in seeds)
+{
+    locations.Add(ProcessLocations(seed));
 }
 
 Console.WriteLine(locations.Min());
+// 51580674 correct
+
+List<long> locations2 = new();
+var seedCouples = new List<(long Start, long Count)>();
+for (int i = 0; i < seeds.Count / 2; i++)
+{
+    seedCouples.Add(
+        (seeds[i], seeds[i + 1])
+    );
+}
+
+// This is very slow and also very stupid
+// 3 scenario's
+// 1 - range is completely outside seed-ranges => keep range as is (lucky)
+// 2 - range is completely in seed-range => map range to new range (lucky)
+// 3 - range is partially in seed-ranges => split range into sub-range
+foreach (var seed in seedCouples)
+{
+    Console.WriteLine($"Range {seed}");
+    for (long i = seed.Start; i < seed.Start + seed.Count; i++)
+    {
+        // var lastStage = stages.LastOrDefault();
+        var lastInputOutputs = new List<(long Input, long Output)> { (Input: i, Output: 0L) };
+        seedIO.Add(lastInputOutputs);
+
+        foreach (var mapping in mappings)
+        {
+            var lastInputOutput = lastInputOutputs.Last();
+            var lastInput = lastInputOutput.Input;
+            var lessThanMapping = mapping
+                .Mapping.FirstOrDefault(m =>
+                    m.Source <= lastInput && lastInput < m.Source + m.Count
+                );
+
+            var conversion = lessThanMapping != default
+                ? lessThanMapping.Target + (lastInput - lessThanMapping.Source)
+                : lastInput;
+            lastInputOutput!.Output = conversion;
+            lastInputOutputs[^1] = lastInputOutput;
+            lastInputOutputs = lastInputOutputs.Append((conversion, 0L)).ToList();
+            if (mapping.Link.Target == "location" || mapping.Link.Target == "soil")
+            {
+                if (mapping.Link.Target == "location")
+                {
+                    locations2.Add(lastInputOutput.Output);
+                    break;
+                }
+
+                // Console.WriteLine(
+                //     $"{mapping.Link.Source} {lastInputOutput.Input}, {mapping.Link.Target} {lastInputOutput.Output}");
+            }
+        }
+    }
+}
+Console.WriteLine(locations2.Min());
 
 public class Map
 {
