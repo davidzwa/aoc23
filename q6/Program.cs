fc1877b9f6e4b7eb245d5feb9c8aead74cafdd2d
David Zwart <davidzwa@gmail.com>
2023-12-19 23:05:31 +0100

    feat: parse q6

---
+++ q6/Program.cs
@@ -0,0 +1,28 @@
+﻿var files = new[]
+{
+    "input.txt",
+    "example.txt",
+    "test1.txt",
+    "input2.txt",
+};
+string filePath = files[0];
+List<string> fileContent = File.ReadLines(filePath).ToList();
+
+var times = new List<int>();
+var distances = new List<int>();
+foreach (var line in fileContent)
+{
+    if (line.Contains("Time"))
+    {
+        times = line.Replace("Time:", "")
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+    }
+    else if (line.Contains("Distance"))
+    {
+        distances = line.Replace("Distance:", "")
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+    }
+}
+
+times.ForEach(Console.WriteLine);
+distances.ForEach(Console.WriteLine);
