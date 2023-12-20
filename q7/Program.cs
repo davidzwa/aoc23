f104340eb89fc62ed424cbebaeb8adeebf66231e
David Zwart <davidzwa@gmail.com>
2023-12-20 22:03:41 +0100

    feat: analyze cards

--- q7/Program.cs
+++ q7/Program.cs
@@ -5,16 +5,11 @@ var files = new[]
     "input.txt",
     "example.txt",
     "test1.txt",
-    "input2.txt",
 };
-string filePath = files[1];
+string filePath = files[2];
 List<string> fileContent = File.ReadLines(filePath).ToList();
 
-var chars =
-    "A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, 2".Split(", ", StringSplitOptions.RemoveEmptyEntries)
-        .ToList();
 
-var dict = Enumerable.Reverse(chars).Select((string c, int d) => new { c, d }).ToDictionary(c => c.c, c => c.d);
 // Console.WriteLine(dict);
 
 List<(string Hand, long Bid, State state)> plays = new();
--- q7/Question.cs
+++ q7/Question.cs
@@ -1,19 +1,67 @@
-﻿namespace q7;
+﻿using System.Text.RegularExpressions;
+
+namespace q7;
 
 public class Question
 {
+    public static List<string> Chars =
+        "A, K, Q, J, T, 9, 8, 7, 6, 5, 4, 3, 2".Split(", ", StringSplitOptions.RemoveEmptyEntries)
+            .ToList();
+
+    public static Dictionary<string, int> Scores = Enumerable.Reverse(Chars).Select((string c, int d) => new { c, d })
+        .ToDictionary(c => c.c, c => c.d);
+
     public static void AnalyzeLines(ref List<(string Hand, long Bid, State State)> plays)
     {
         foreach (var play in plays)
         {
             play.State.Type = AnalyzeLine(play.Hand);
+            Console.WriteLine($"Line {play.Hand} Type {play.State.Type}");
         }
-        Console.WriteLine(plays.First().State.Type);
+
+        // Console.WriteLine(plays.First().State.Type);
     }
-    
-    public static  HandType AnalyzeLine(string line)
+
+    public static HandType AnalyzeLine(string line)
     {
+        var dict = new Dictionary<string, int>();
+        foreach (var c in Chars)
+        {
+            var count = Regex.Count(line, c);
+            dict[c] = count;
+        }
+
+        var max = dict.Max(d => d.Value);
+        if (max == 5)
+        {
+            return HandType.FiveOfAKind;
+        }
+
+        if (max == 4)
+        {
+            return HandType.FourOfAKind;
+        }
+
+        if (dict.Any(d => d.Value == 3) && dict.Any(d => d.Value == 2))
+        {
+            return HandType.FullHouse;
+        }
+
+        if (max == 3)
+        {
+            return HandType.ThreeOfAKind;
+        }
+
+        if (max == 2 && dict.Count(v => v.Value == 2) == 2)
+        {
+            return HandType.TwoPair;
+        }
         
-        return HandType.FullHouse;
+        if (max == 2 && dict.Count(v => v.Value == 2) == 1)
+        {
+            return HandType.OnePair;
+        }
+
+        return HandType.HighCard;
     }
 }
