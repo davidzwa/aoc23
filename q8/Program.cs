2726f9692922390806dd403bf7628f095b7f6400
David Zwart <davidzwa@gmail.com>
2023-12-24 21:13:09 +0100

    simplify

--- q8/Program.cs
+++ q8/Program.cs
@@ -23,29 +23,22 @@ var steps = 0;
 var source = map.First(f => f.Key == "AAA");
 for (int j = 0; j < directions.Count; j++)
 {
-    
     if (Question.IsDone(source.Key))
     {
         break;
     }
 
-    var direction = Question.ParseDirection(directions[j]);
-    Console.WriteLine($"[{steps}] Source: {source.Key} Dirs: {source.Value} Next: {direction}");
-
-    var targetName = Question.ProcessLine(source.Value, direction);
-    source = new (targetName, map[targetName]);
-
+    var targetName = Question.ProcessSourceDirection(directions[j], source.Value);
+    source = new(targetName, map[targetName]);    
+    Console.WriteLine($"[{steps}] Source: {source.Key} Dirs: {source.Value} Next: {targetName}");
+    
     steps++;
     if (j == directions.Count - 1)
     {
         j = -1;
     }
-
-    // if (steps > 50)
-    // {
-    //     throw new Exception("Ehm");
-    // }
 }
 
 Console.WriteLine($"Steps {steps}");
 // 5041 too low
+// 19951 correct
--- q8/Question.cs
+++ q8/Question.cs
@@ -4,7 +4,21 @@ public class Question
 {
     public static bool V2 = false;
 
-    
+    public static string ProcessSourceDirection(char directionStr, (string Left, string Right) options)
+    {
+        var direction = ParseDirection(directionStr);
+        return ProcessDirection(options, direction);
+    }
+
+    public static string ProcessDirection((string Left, string Right) directions, Direction instruction)
+    {
+        if (instruction == Direction.Left)
+        {
+            return directions.Left;
+        }
+
+        return directions.Right;
+    }
 
     public static Direction ParseDirection(char dirChar)
     {
@@ -14,15 +28,5 @@ public class Question
     public static bool IsDone(string source)
     {
         return source == "ZZZ";
-    }
-
-    public static string ProcessLine((string Left, string Right) directions, Direction instruction)
-    {
-        if (instruction == Direction.Left)
-        {
-            return directions.Left;
-        }
-
-        return directions.Right;
     }
 }
