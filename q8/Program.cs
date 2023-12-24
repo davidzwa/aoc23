156b17cbda17f70080d7f3e185e172a75a58e1b0
David Zwart <davidzwa@gmail.com>
2023-12-24 21:23:11 +0100

    simplify

--- q8/Program.cs
+++ q8/Program.cs
@@ -1,4 +1,5 @@
-﻿using q8;
+﻿using System.Diagnostics;
+using q8;
 
 Question.V2 = false;
 var files = new[]
@@ -28,10 +29,9 @@ for (int j = 0; j < directions.Count; j++)
         break;
     }
 
-    var targetName = Question.ProcessSourceDirection(directions[j], source.Value);
-    source = new(targetName, map[targetName]);    
-    Console.WriteLine($"[{steps}] Source: {source.Key} Dirs: {source.Value} Next: {targetName}");
-    
+    source = ProcessNext(ref map, source.Value, directions[j]);
+    Console.WriteLine($"[{steps}] Source: {source.Key} Dirs: {source.Value} Next: {source.Key}");
+
     steps++;
     if (j == directions.Count - 1)
     {
@@ -42,3 +42,13 @@ for (int j = 0; j < directions.Count; j++)
 Console.WriteLine($"Steps {steps}");
 // 5041 too low
 // 19951 correct
+
+KeyValuePair<string, (string Left, string Right)> ProcessNext(
+    ref Dictionary<string, (string Left, string Right)> map,
+    (string Left, string Right) options,
+    char direction
+)
+{
+    var targetName = Question.ProcessSourceDirection(direction, options);
+    return new(targetName, map[targetName]);
+}
