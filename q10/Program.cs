47c78f78b1d923234df295d5100ca4289edc253b
David Zwart <davidzwa@gmail.com>
2024-01-02 08:23:54 +0100

    finish part 1

--- q10/Program.cs
+++ q10/Program.cs
@@ -6,20 +6,20 @@ var files = new[]
     "example.txt",
     "test1.txt",
 };
-string filePath = files[1];
+string filePath = files[0];
 List<string> fileContent = File.ReadLines(filePath).ToList();
 
-List<List<(Pipe Direction, int Score)>> directions = new();
+List<List<(Pipe Pipe, int Score)>> directions = new();
 var startPosition = Question.ParsePipes(ref directions, fileContent);
-
-Console.WriteLine(startPosition.X + " " + startPosition.Y);
+var size = (W: directions.First().Count, H: directions.Count);
+Console.WriteLine($"Start {startPosition.X}:{startPosition.Y} W:{size.W} H:{size.H}");
 
 // Breadth-first, at start create list of current moves
 // list valid options, save incremented score to all if existing score is 0 or if non-zero + smaller, add to moves-list if score is zero
 // remove current move
 // iterate moves list until no moves
 
-int movesLeft = 10;
+int movesLeft = 50000;
 var currentPosition = startPosition;
 List<Move> moves = new()
 {
@@ -36,11 +36,46 @@ while (movesLeft-- > 0)
     currentPosition = moves.First().To;
     if (movesLeft % 5 == 0)
     {
-        Console.WriteLine($"MovesLeft {movesLeft}");
+        Console.WriteLine($"MovesLeft {movesLeft} Moves gathered {moves.Count}");
     }
 
     // Analyze moves from current position
-    var newMoves = Question.GetValidMoves(ref directions, currentPosition);
+    var newMoves = Question.GetValidMoves(directions, currentPosition);
+    // Clear current move, add new ones to list
+    moves.RemoveAt(0);
+    moves.AddRange(newMoves.Where(nm =>
+    {
+        var target = directions[nm.To.Y][nm.To.X];
+        return target.Score == 0 && target.Pipe != Pipe.Start;
+    }));
+    
+    // Increment scores
+    foreach (var m in newMoves)
+    {
+        var pos = m.From;
+        var toPos = m.To;
+        var direction = directions[pos.Y][pos.X];
+        var toDirection = directions[toPos.Y][toPos.X];
+        Console.WriteLine(
+            $"{direction.Pipe.ToChar()}({pos.X}:{pos.Y})={direction.Score} {toDirection.Pipe.ToChar()}({toPos.X}:{toPos.Y})={toDirection.Score}");
+
+        if (toDirection.Pipe == Pipe.Start)
+        {
+            // Console.WriteLine("Returned to start, not incrementing");
+        }
+        else if (toDirection.Score != 0 && toDirection.Score <= direction.Score + 1)
+        {
+            // Console.WriteLine("ToDirection score != 0 and not smaller than proposed route");
+        }
+        else
+        {
+            // Console.WriteLine("Incrementing score");
+            directions[toPos.Y][toPos.X] = (directions[toPos.Y][toPos.X].Pipe, direction.Score + 1);
+        }
+    }    
+
+    
+    Console.WriteLine($"\nGot +{newMoves.Count} new moves from {currentPosition}, total: {moves.Count}");
 
     if (moves.Count == 0)
     {
@@ -49,10 +84,21 @@ while (movesLeft-- > 0)
     }
 }
 
-Console.WriteLine($"\nMax {directions.Max(d => d.Max())}");
+for (int i = 0; i < size.H; i++)
+{
+    for (int j = 0; j < size.W; j++)
+    {
+        var repr = directions[i][j].Score != 0 ? " " + directions[i][j].Score: " .";
+        Console.Write(repr);
+    }
+    Console.WriteLine();
+}
 
+Console.WriteLine($"\nMax {directions.Max(d => d.Max(sp => sp.Score))}");
+// 16 is wrong
+// 6831 is correct
 public class Move
 {
-    public (int x, int y) From { get; set; }
-    public (int x, int y) To { get; set; }
+    public (int X, int Y) From { get; set; }
+    public (int X, int Y) To { get; set; }
 }
--- q10/Question.cs
+++ q10/Question.cs
@@ -1,11 +1,11 @@
 ﻿namespace q10;
 
-public class Question
+public static class Question
 {
-    public static (int X, int Y) ParsePipes(ref List<List<(Pipe Direction, int Score)>> directions, List<string> lines)
+    public static (int X, int Y) ParsePipes(ref List<List<(Pipe Pipe, int Score)>> directions, List<string> lines)
     {
         directions.Clear();
-        
+
         (int X, int Y) startPosition = (-1, -1);
         foreach (var (y, line) in lines.Select((value, index) => (index, value)))
         {
@@ -28,28 +28,93 @@ public class Question
         {
             throw new Exception("Start position not found");
         }
-        
+
         return startPosition;
     }
 
-    public static List<Move> GetValidMoves(ref List<List<(Pipe Direction, int Score)>> directions,
+    public static List<Move> GetValidMoves(List<List<(Pipe Pipe, int Score)>> directions,
         (int X, int Y) pos)
     {
         var size = (W: directions.First().Count, H: directions.Count);
-        var newMoves = new List<Move>();
-        
-        // TODO check top(if pos.y > 0), left(if pos is >0),right(if pos is <width-1), bottom (if pos.y < h-1)  
-        pos.X
-        
-        return newMoves;
+
+        var direction = directions[pos.Y][pos.X];
+        var absOptions = ToPipeOptions(direction.Pipe).Select(o => (X: pos.X + o.X, Y: pos.Y + o.Y)).ToList();
+        var relevantOptions = absOptions.Where(c =>
+        {
+            var wMin = c.X >= 0;
+            var wMax = c.X < size.W;
+            var hMin = c.Y >= 0;
+            var hMax = c.Y < size.H;
+            var isInRect = wMin && wMax && hMin && hMax;
+            if (!wMax || !hMax)
+            {
+                Console.WriteLine(
+                    $"W:{size.W} H:{size.H} {ToChar(direction.Pipe)}({pos.X}:{pos.Y}) ({c.X}:{c.Y}) Invalid");
+            }
+
+            return isInRect;
+        }).ToList();
+
+        return relevantOptions
+            .Where(roPos =>
+            {
+                var toDirectionOption = directions[roPos.Y][roPos.X];
+                var targets = ToPipeOptions(toDirectionOption.Pipe).Select(o => (X: roPos.X + o.X, Y: roPos.Y + o.Y))
+                    .ToList();
+                // The connecting pipe must have a target that is equal to the current position we're trying to move from
+                var overlaps = targets.Any(t => t == pos);
+                return overlaps;
+            })
+            .Select(ro => new Move()
+            {
+                From = pos,
+                To = ro,
+            }).ToList();
     }
 
-    static bool IsValidMove(Pipe from, Pipe to, Direction direction)
+    public static List<(int X, int Y)> ToPipeOptions(Pipe pipe) => pipe switch
+    {
+        Pipe.Vert => new()
+        {
+            (0, -1),
+            (0, 1)
+        },
+        Pipe.Hor => new()
+        {
+            (-1, 0),
+            (1, 0)
+        },
+        Pipe.NorthEastL => new()
+        {
+            (0, -1),
+            (1, 0)
+        },
+        Pipe.NorthWestJ => new()
+        {
+            (0, -1),
+            (-1, 0)
+        },
+        Pipe.SouthWest7 => new()
+        {
+            (0, 1),
+            (-1, 0)
+        },
+        Pipe.SouthEastF => new()
+        {
+            (0, 1),
+            (1, 0)
+        },
+        Pipe.Ground => new(),
+        Pipe.Start => new List<Pipe>() { Pipe.Hor, Pipe.Vert }.SelectMany(ToPipeOptions).ToList(),
+        _ => throw new Exception("Pipe not recognized"),
+    };
+
+    public static bool IsValidMove(Pipe from, Pipe to, Direction direction)
     {
         return true;
-    } 
-    
-    static Pipe ToPipe(char pipe) => pipe switch
+    }
+
+    public static Pipe ToPipe(this char pipe) => pipe switch
     {
         '|' => Pipe.Vert,
         '-' => Pipe.Hor,
@@ -62,9 +127,24 @@ public class Question
         _ => throw new Exception("Pipe not recognized"),
     };
 
+    public static char ToChar(this Pipe pipe) => pipe switch
+    {
+        Pipe.Vert => '|',
+        Pipe.Hor => '-',
+        Pipe.NorthEastL => 'L',
+        Pipe.NorthWestJ => 'J',
+        Pipe.SouthWest7 => '7',
+        Pipe.SouthEastF => 'F',
+        Pipe.Ground => '.',
+        Pipe.Start => 'S',
+        _ => throw new Exception("Pipe not recognized"),
+    };
+
     public enum Direction
     {
         Top,
-        Bottom,Left,Right
+        Bottom,
+        Left,
+        Right
     }
 }
