dad77d9645910f3eda6fcbffb6c3bb86171e24be
David Zwart <davidzwa@gmail.com>
2024-01-06 22:42:14 +0100

    feat: parse q12

--- q12/Program.cs
+++ q12/Program.cs
@@ -4,5 +4,29 @@
     "example.txt",
     "test1.txt",
 };
-string filePath = files[0];
+string filePath = files[1];
 List<string> fileContent = File.ReadLines(filePath).ToList();
+
+List<Row> rows = new();
+foreach (var line in fileContent)
+{
+    var springRecord = line.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    var springs = springRecord[0];
+    var record = springRecord[1].Split(",", StringSplitOptions.RemoveEmptyEntries);
+    rows.Add(new ()
+    {
+        Chars = springs.ToList(),
+        Record = record.Select(int.Parse).ToList()
+    });
+    
+    Console.Write(springs + " ");
+    Console.WriteLine(string.Join(", ", record));
+}
+
+class Row
+{
+    public List<char> Chars { get; set; }
+    
+    public List<int> Record { get; set; }
+    
+}
